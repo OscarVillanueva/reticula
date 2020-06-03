9039b012d47c2ba0bcf3e6941f4449aa153e484b
root <javiondo68@gmail.com>
2020-06-03 14:13:39 -0500

    Modal done

--- js/gen.js
+++ js/gen.js
@@ -6,6 +6,9 @@
 // Seleccionamos el boton que genera la carga
 const button = document.querySelector('#gen')
 
+// Seleccionamos el boton para cerrar el modal
+const close = document.querySelector('span.close')
+
 //Funcion recuperar datos materia
 function recmat (mate) {
 	this.nom = mate.querySelector("#nombre").innerHTML;
@@ -123,9 +126,11 @@ button.addEventListener('click', async e => {
 	
 	// Este arreglo tiene las materias en un arreglo para que se ha más facil mostrar en pantalla
 	const propuesta = await fetchSubjects(carga)
+	loadDataToModal(propuesta)
+	showModal()
 
-	console.log(propuesta);
-    console.log("Generando . . . ");  
+	// console.log(propuesta);
+    // console.log("Generando . . . ");  
 })
 
 // Cargar las materias originales
@@ -136,7 +141,12 @@ const fetchSubjects = async carga => {
 	const { Materias } = await response.json()
 
 	// Sacamos los ids seleeccionados
-	const ids = carga.map(subject => subject.getAttribute("data-id"))
+	const ids = carga.map(subject => {
+		return {
+			id: subject.getAttribute("data-id"),
+			chance: subject.getAttribute("data-type")
+		}
+	})
 
 	// Propuesta
 	const propuesta = []
@@ -144,12 +154,108 @@ const fetchSubjects = async carga => {
 	ids.forEach( id => {
 
 		// Buscamos la materia entre todas las materias
-		const subject = Materias.find( subject => subject.clave === id )
+		const subject = Materias.find( subject => subject.clave === id.id )
 
 		// Si la encuentra la agrega al arreglo de propuesta
-		if(subject) propuesta.push( subject )
+		if(subject) propuesta.push({ ...subject, chance: id.chance })
 
 	});
 
 	return propuesta;
+}
+
+// Mostrar el modal
+const showModal = () => {
+	
+	const modal = document.querySelector('.modal')
+	modal.style.display = "block"
+
+}
+
+// Cerrar el modal
+close.addEventListener('click', () => {
+	
+	const modal = document.querySelector('.modal')
+	modal.style.display = "none"
+	
+})
+
+// Cargar la información al modal
+const loadDataToModal = subjects => {
+
+	// Seleccionamos el modal 
+	const modal = document.querySelector('.modal')
+
+	// Seleccionamos el contenedor de la información
+	const content = modal.querySelector(".modal-info")
+
+	// Construimos lo que se va a mostrar
+	const totalCredits = subjects.reduce( (total, subject) => total + Number(subject.creditos), 0 )
+
+	// Creamos las filas de las materias
+	let rows = ""
+	subjects.forEach(subject => {
+		
+		rows = `${rows} <tr>
+				<td class = "border px-4 py-2">
+					${subject.nombre}
+				</td>
+				<td class = "border px-4 py-2">
+					${subject.creditos}
+				</td>
+				<td class = "border px-4 py-2">
+					${subject.semestre}
+				</td>
+				<td class = "border px-4 py-2">
+					${ translate( subject.chance ) }
+				</td>
+			</tr>
+		`
+
+	});
+
+	// Template para mostrar las materias
+	const template = `
+		<h1 class = "uppercase font-bold text-2xl text-center">Propuesta de carga Académica</h1>
+		<h3 class = "uppercase font-bold text-lg text-center">Creditos totales: ${totalCredits}</h3>
+		<div>
+			<table class="table-auto w-full my-6">
+				<thead>
+					<tr>
+						<th class="px-4 py-2">Materia</th>
+						<th class="px-4 py-2">Creditos</th>
+						<th class="px-4 py-2">Semestre</th>
+						<th class="px-4 py-2">Oportunidad</th>
+					</tr>
+				</thead>
+				<tbody>
+					${rows}
+				</tbody>
+			</table>
+		</div>
+	`
+
+	content.innerHTML = template
+}
+
+// Pasar de la clave de oportunidad 
+// normal -> primera oportunidad
+// second -> Repeticion
+// third -> Especial
+const translate = chance => {
+	
+	switch ( chance ) {
+		case "normal":
+			return "Primera oportunidad"
+
+		case "second":
+			return "Segunda oportunidad"
+
+		case "third":
+			return "Especial"
+	
+		default:
+			break;
+	}
+
 }
